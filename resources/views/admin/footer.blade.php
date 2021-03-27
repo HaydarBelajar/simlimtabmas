4ee38af57f45c5d276556d968126d2e0ba4e56a0
danurwijayanto <danurwijayanto@gmail.com>
2021-03-27 11:09:52 +0700

    fix logo

--- resources/views/admin/footer.blade.php
+++ resources/views/admin/footer.blade.php
@@ -6,8 +6,16 @@
     <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights
     reserved.
 
-    @stack('scripts')
   </footer>
+  <!-- jQuery -->
+  <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
+  <!-- jQuery UI 1.11.4 -->
+  <script src="{{ asset('assets/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+  <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+  <script>
+    $.widget.bridge('uibutton', $.ui.button)
+  </script>
+
   <!-- Bootstrap 4 -->
   <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
   <!-- ChartJS -->
@@ -34,7 +42,8 @@
   <script src="{{ asset('assets/dist/js/demo.js') }}"></script>
   <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
   <script src="{{ asset('assets/dist/js/pages/dashboard.js') }}"></script>
-
+  
   <!-- App scripts -->
+  @stack('scripts')
 </body>
 @endsection
--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -2,7 +2,9 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Dashboard</title>
+  <title>Simlitabmas | UNISA</title>
+
+  <link rel="shortcut icon" href="{{ asset('assets/images/ristekBrin.ico') }}" type="image/x-icon" /><link rel="icon" href="Images/icon/ristekBrin.ico" type="image/x-icon" />
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
