4e176335c7d1d3e0aee82a4f4d44cc2692c12d4c
danurwijayanto <danurwijayanto@gmail.com>
2021-06-26 11:07:39 +0700

    hadeh, cuman button

--- resources/views/admin/content/sub-content/home-sinta.blade.php
+++ resources/views/admin/content/sub-content/home-sinta.blade.php
@@ -1,5 +1,116 @@
-Pellentesque vestibulum commodo nibh nec blandit. Maecenas neque magna, iaculis tempus turpis ac, ornare
-sodales tellus. Mauris eget blandit dolor. Quisque tincidunt venenatis vulputate. Morbi euismod molestie
-tristique. Vestibulum consectetur dolor a vestibulum pharetra. Donec interdum placerat urna nec pharetra.
-Etiam eget dapibus orci, eget aliquet urna. Nunc at consequat diam. Nunc et felis ut nisl commodo dignissim.
-In hac habitasse platea dictumst. Praesent imperdiet accumsan ex sit amet facilisis.
+<div class="card">
+  <div class="card-header">
+    <h5 class="card-header-text"></h5>
+    <a class="btn btn-primary float-sm-right" href="">Sinkronisasi</a>
+  </div>
+  <div class="card-block">
+    <div class="view-info">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="general-info">
+            <div class="row">
+              <div class="col-lg-12 col-xl-6">
+                <table class="table">
+                  <tbody>
+                    <tr>
+                      <th scope="row" class="auto-style1">Sinta ID</th>
+                      <td>
+                        <span id="ContentPlaceHolder1_ctl00_sinta_lblSintaId" style="color:Green;">6013452</span>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th scope="row" class="auto-style1">Sinta Skor</th>
+                      <td>
+                        <span id="ContentPlaceHolder1_ctl00_sinta_lblSintaSkor" style="color:Green;">1.12</span>
+                      </td>
+                    </tr>
+
+                  </tbody>
+                </table>
+              </div>
+              <!-- end of table col-lg-6 -->
+
+              <!-- end of table col-lg-6 -->
+            </div>
+            <!-- end of row -->
+            <div class="row">
+              <table class="table m-0">
+                <tbody>
+                  <tr>
+                    <th scope="row" style="align-content: center"></th>
+                    <td>
+                      <!--<span id="ContentPlaceHolder1_ctl00_sinta_Label1" style="color:Blue;">Scopus</span>-->
+                      <span class="btn btn-block btn-primary btn-lg" style="font-size: large">Scopus</span>
+                    </td>
+                    <th scope="row" style="align-content: center"></th>
+                    <td>
+                      <!--<span id="ContentPlaceHolder1_ctl00_sinta_Label2" style="color:Blue;">Google Scholar</span>-->
+                      <span class="btn btn-block btn-primary btn-lg" style="font-size: large">Google
+                        Scholar</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <table class="table m-0">
+                <tbody>
+
+                  <tr>
+                    <th scope="row">Scopus Id </th>
+                    <td>
+                      <span id="ContentPlaceHolder1_ctl00_sinta_lblScopusid" style="color:Green;">57218794916</span>
+                    </td>
+                    <th scope="row">Google ID</th>
+                    <td>
+                      <span id="ContentPlaceHolder1_ctl00_sinta_lblGooglescholarid"
+                        style="color:Green;">Ew_wSykAAAAJ</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">H-Index</th>
+                    <td>
+                      <span id="ContentPlaceHolder1_ctl00_sinta_lblScopushindex" style="color:Green;">0</span>
+                    </td>
+                    <th scope="row">H-Index</th>
+                    <td>
+                      <span id="ContentPlaceHolder1_ctl00_sinta_lblGooglehindex" style="color:Green;">5</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Articles</th>
+                    <td>
+                      <span id="ContentPlaceHolder1_ctl00_sinta_lblScopusArticle" style="color:Green;">1</span>
+                    </td>
+                    <th scope="row">Articles</th>
+                    <td>
+                      <span id="ContentPlaceHolder1_ctl00_sinta_lblGoogleArticle" style="color:Green;">120</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Citation</th>
+                    <td>
+                      <span id="ContentPlaceHolder1_ctl00_sinta_lblScopusCitation" style="color:Green;">0</span>
+                    </td>
+                    <th scope="row">Citation</th>
+                    <td>
+                      <span id="ContentPlaceHolder1_ctl00_sinta_LblGoogleCitation" style="color:Green;">75</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row"></th>
+                    <td>
+                      <span id="ContentPlaceHolder1_ctl00_sinta_Label3" style="color:Green;"></span>
+                    </td>
+                    <th scope="row">Google I10</th>
+                    <td>
+                      <span id="ContentPlaceHolder1_ctl00_sinta_lblGoogleI10" style="color:Green;">1</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
