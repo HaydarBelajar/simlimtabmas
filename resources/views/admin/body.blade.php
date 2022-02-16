388c399b292914d78eccc9ea806ef5514fbdd150
danurwijayanto <danurwijayanto@gmail.com>
2022-02-16 09:59:48 +0700

    backup

--- resources/views/admin/body.blade.php
+++ resources/views/admin/body.blade.php
@@ -4,10 +4,10 @@
   <div class="wrapper">
 
     <!-- Preloader -->
-    {{-- <div class="preloader flex-column justify-content-center align-items-center">
-      <img class="animation__shake" src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60"
-        width="60">
-    </div> --}}
+    <div class="preloader flex-column justify-content-center align-items-center">
+      <img class="animation__shake" src="{{ asset('assets/images/cropped-logo-unisa-crop.png') }}" alt="AdminLTELogo"
+        height="60" width="60">
+    </div>
 
     <!-- Navbar -->
     <nav class="main-header navbar navbar-expand navbar-white navbar-light">
@@ -15,6 +15,9 @@
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+          <a href="{{ route('dashboard.home') }}" class="nav-link">{{ env('APP_NAME') }}</a>
         </li>
         {{-- <li class="nav-item d-none d-sm-inline-block">
           <a href="index3.html" class="nav-link">Home</a>
@@ -49,7 +52,8 @@
         </li> --}}
 
         <!-- Messages Dropdown Menu -->
-        {{-- <li class="nav-item dropdown">
+        {{--
+        <li class="nav-item dropdown">
           <a class="nav-link" data-toggle="dropdown" href="#">
             <i class="far fa-comments"></i>
             <span class="badge badge-danger navbar-badge">3</span>
@@ -89,6 +93,7 @@
               <!-- Message End -->
             </a>
             <div class="dropdown-divider"></div>
+
             <a href="#" class="dropdown-item">
               <!-- Message Start -->
               <div class="media">
@@ -109,6 +114,7 @@
             <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
           </div>
         </li>
+        --}}
         <!-- Notifications Dropdown Menu -->
         <li class="nav-item dropdown">
           <a class="nav-link" data-toggle="dropdown" href="#">
@@ -136,6 +142,7 @@
             <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
           </div>
         </li>
+        {{--
         <li class="nav-item">
           <a class="nav-link" data-widget="fullscreen" href="#" role="button">
             <i class="fas fa-expand-arrows-alt"></i>
@@ -144,8 +151,8 @@
         <li class="nav-item">
           <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
             <i class="fas fa-th-large"></i>
-          </a>
-        </li> --}}
+          </a>--}}
+        </li>
       </ul>
     </nav>
     <!-- /.navbar -->
