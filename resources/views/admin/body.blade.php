1e05aef7732e110a122bfd0a539f1ce327c31d7c
danurwijayanto <danurwijayanto@gmail.com>
2022-02-17 11:38:40 +0700

    notifikasi sukses

--- resources/views/admin/body.blade.php
+++ resources/views/admin/body.blade.php
@@ -119,28 +119,27 @@
         <li class="nav-item dropdown">
           <a class="nav-link" data-toggle="dropdown" href="#">
             <i class="far fa-bell"></i>
-            <span class="badge badge-warning navbar-badge">15</span>
+            @if(Session::has('kucingku'))
+            <span class="badge badge-warning navbar-badge">{{ count(Session::get('kucingku')['notification'])
+              }}</span>
+            @endif
           </a>
+          @if(Session::has('kucingku'))
           <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-            <span class="dropdown-item dropdown-header">15 Notifications</span>
+            <span class="dropdown-item dropdown-header">Jumlah Notifikasi : {{
+              count(Session::get('kucingku')['notification']) }} </span>
+            @foreach (Session::get('kucingku')['notification'] as $item)
             <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
-              <i class="fas fa-envelope mr-2"></i> 4 new messages
-              <span class="float-right text-muted text-sm">3 mins</span>
+            <a href=<?=$item['tipe']=='penelitian' ? "/penelitian/detail-penelitian/" .$item['id'] : "#" ?> class="
+              dropdown-item">
+              <i class="fas fa-envelope mr-2"></i>{{$item['tipe']}} : {{$item['judul']}}
+              {{-- <span class="float-right text-muted text-sm">3 mins</span> --}}
             </a>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
-              <i class="fas fa-users mr-2"></i> 8 friend requests
-              <span class="float-right text-muted text-sm">12 hours</span>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
-              <i class="fas fa-file mr-2"></i> 3 new reports
-              <span class="float-right text-muted text-sm">2 days</span>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+            {{-- <div class="dropdown-divider"></div> --}}
+            {{-- <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a> --}}
+            @endforeach
           </div>
+          @endif
         </li>
         {{--
         <li class="nav-item">
